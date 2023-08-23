85bbf6c0dcca1b74ec0b4ac68e8144e7ed0334a2
lenny1007 <78573737+lenny1007@users.noreply.github.com>
2023-08-23 17:05:12 +0800

    -minor fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,35 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  devServer: {
+    proxy: {
+      /*
+      '/v1':{
+        target: 'https://dictionary-api-4rbqr7xcwq-de.a.run.app',
+        pathRewrite: {'^/v1':''},
+        changeOrigin: true,
+        ws: true
+      },*/
+      
+      '/v1': {
+        target: 'https://dictionary-api-4rbqr7xcwq-de.a.run.app/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/v1/, ''),
+      },
+      configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, options.target + proxyReq.path);
+          });
+
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Receiving Response from the Target:', req.method, options.target + req.url);
+          });
+
+          proxy.on('error', (err, req, res) => {
+            console.log('Error Occurred:', err);
+          });
+      }
+    }
+  }
 })
