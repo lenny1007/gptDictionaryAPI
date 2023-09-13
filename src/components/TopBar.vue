3f560dcee314e4094a1058f62681f9c7f77a922d
lenny1007 <78573737+lenny1007@users.noreply.github.com>
2023-09-13 11:09:23 +0800

    top bar link update

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -15,19 +15,15 @@ const appTitle =  "GPT Dictionary API"
       <v-toolbar-items class="d-none d-sm-flex fill-height">
         <v-btn href="https://paypal.me/lennychen0929?country.x=TW&locale.x=zh_TW" stacked><v-icon class="ma-1 fab fa-paypal"></v-icon> Donate </v-btn>
         <v-btn href="https://www.buymeacoffee.com/lenny0929/extras" stacked><v-icon class="ma-1 fas fa-circle-dollar-to-slot"></v-icon> Download  </v-btn>
-
-        <!--<font-awesome-icon :icon="['fas', 'mug-saucer']" />
-        <v-btn stacked><v-icon class="ma-1 fab fa-github"></v-icon> Github  </v-btn>
-        -->
+        <v-btn href="https://github.com/lenny1007/gptDictionaryAPI" stacked><v-icon class="ma-1 fab fa-github"></v-icon> Github  </v-btn>
         <v-btn href="mailto:lenny0929@gmail.com" stacked><v-icon class="ma-1 fas fa-envelope"></v-icon> Email  </v-btn>
       
       </v-toolbar-items>
       <v-toolbar-items class="d-flex d-sm-none fill-height" >
-        <v-btn ><v-icon size="xx-large" class="ma-1 fab fa-paypal"></v-icon></v-btn>
-        <!--
-        <v-btn ><v-icon size="xx-large" class="ma-1 fab fa-github"></v-icon></v-btn>
-        -->
-        <v-btn ><v-icon size="xx-large" class="ma-1 fas fa-envelope"></v-icon></v-btn>
+        <v-btn href="https://paypal.me/lennychen0929?country.x=TW&locale.x=zh_TW"><v-icon size="xx-large" class="ma-1 fab fa-paypal"></v-icon></v-btn>
+        <v-btn href="https://www.buymeacoffee.com/lenny0929/extras"><v-icon size="xx-large" class="ma-1 fas fa-circle-dollar-to-slot"></v-icon></v-btn>
+        <v-btn href="https://github.com/lenny1007/gptDictionaryAPI"><v-icon size="xx-large" class="ma-1 fab fa-github"></v-icon></v-btn>
+        <v-btn href="mailto:lenny0929@gmail.com"><v-icon size="xx-large" class="ma-1 fas fa-envelope"></v-icon></v-btn>
       </v-toolbar-items>
     </v-container>  
     </v-toolbar>
